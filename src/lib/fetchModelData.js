be3f748ad1c13ec8996f98b413d9b38ba9be62cd
MANH NGUYEN DUC <135792849+manhwork@users.noreply.github.com>
2025-05-18 08:07:39 +0000

    a

--- src/lib/fetchModelData.js
+++ src/lib/fetchModelData.js
@@ -5,19 +5,19 @@
  * @returns {Promise}       Promise that resolves to the response data
  */
 async function fetchModel(url) {
-    try {
-        // const baseUrl = "https://7hjy43-8080.csb.app";
-        const baseUrl = "http://localhost:8081";
-        const response = await fetch(`${baseUrl}${url}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        throw error;
+  try {
+    const baseUrl = "https://mcws8y-8081.csb.app";
+    // const baseUrl = "http://localhost:8081";
+    const response = await fetch(`${baseUrl}${url}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    throw error;
+  }
 }
 
 export default fetchModel;
